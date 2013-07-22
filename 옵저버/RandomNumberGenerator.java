367048a6029d50df39cc62df5ea1e7fb1cf33dee
final567 <final-ff7@hanmail.net>
2013-07-22 16:18:29 +0900

    first commit

---
+++ 옵저버/RandomNumberGenerator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/**
+ * Write a description of class RandomNumberGenerator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RandomNumberGenerator extends NumberGenerator
+{
+    private Random random = new Random();
+    private int number;
+    public int getNumber(){
+        return number;
+    }
+    public void execute(){
+        for(int i = 0; i<10; i++){
+            number = random.nextInt(50);
+            notifyObservers();
+        }
+    }
+}
