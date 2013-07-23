7e47eaf57a90cf91191414341dc90a40bd7664d4
final567 <final-ff7@hanmail.net>
2013-07-23 10:21:17 +0900

    add *

--- 1주차/옵저버,팩토리패턴,컬렉션/팩토리 패턴/20130627/NYStyleVeggiePizza.java
+++
@@ -1,8 +0,0 @@
-public class NYStyleVeggiePizza extends Pizza {
-  public NYStyleVeggiePizza(){
-     name = "베기피자";
-     dough = "크러스트";
-     sauce = "토마토";
-    }
-}
-
--- 3주차/SelectTest/src/C.java
+++
@@ -1,7 +0,0 @@
-
-public class C {
-	public void printText() {
-		System.out.println("안녕하세요 C클래스 입니다.");
-	}
-
-}
